f3e7a29cc39df97fa0c61efbcffa3a44509b6c34
Eber Coaquira <ebrjose@gmail.com>
2021-08-05 19:37:28 -0400

    Add post comment component

---
+++ resources/views/components/post-comment.blade.php
@@ -0,0 +1,19 @@
+<article class="flex bg-gray-100 border border-gray-200 p-6 rounded-xl space-x-4">
+    <div class="flex-shrink-0">
+        <img src="https://i.pravatar.cc/60" alt="" class="rounded-xl">
+    </div>
+    <div>
+        <header class="mb-4">
+            <h3 class="font-bold"> John Doe</h3>
+            <p class="text-xs">
+                Posted
+                <time>8 months ago</time>
+            </p>
+        </header>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Error quos
+            ducimus possimus sapiente voluptatem ab alias nemo quod saepe reiciendis,
+            labore deleniti odio at corrupti aliquam illo, molestiae maxime facilis.
+        </p>
+    </div>
+</article>
