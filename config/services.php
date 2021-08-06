e5c4fb941bc79fbe1b8b6f8015d97064b0bf327b
Eber Coaquira <ebrjose@gmail.com>
2021-08-06 10:54:46 -0400

    Extract a Newsletter Service

--- config/services.php
+++ config/services.php
@@ -31,7 +31,11 @@ return [
     ],
 
     'mailchimp' => [
-        'key' => env('MAILCHIMP_KEY')
+        'key' => env('MAILCHIMP_KEY'),
+        'lists' => [
+            'subscribers' => env('MAILCHIMP_LIST_SUBSCRIBERS')
+        ]
+
     ]
 
 ];
