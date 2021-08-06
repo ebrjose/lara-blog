3a584ff02feb686ab06ff9de40dda2b59b456345
Eber Coaquira <ebrjose@gmail.com>
2021-08-05 23:27:47 -0400

    Activate the comment form

--- resources/views/components/post-comment.blade.php
+++ resources/views/components/post-comment.blade.php
@@ -2,7 +2,7 @@
 <x-panel class="bg-gray-50">
     <article class="flex space-x-4">
         <div class="flex-shrink-0">
-            <img src="https://i.pravatar.cc/60?id={{ $comment->id }}" width="60" height="60" class="rounded-xl">
+            <img src="https://i.pravatar.cc/60?img={{ $comment->user_id }}" width="60" height="60" class="rounded-xl">
         </div>
         <div>
             <header class="mb-4">
